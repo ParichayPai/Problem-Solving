14dff391e0558d7a3c5cf104ff3dcb3428b56756
ParichayPai <parichaypai393@gmail.com>
2020-10-04 01:09:20 +0530

    MaxElementInStack

--- src/com/stackProbs/MaxElementInStack.java
+++ src/com/stackProbs/MaxElementInStack.java
@@ -1,47 +1,35 @@
 package com.stackProbs;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
 public class MaxElementInStack {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        Stack<Long> st = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            String[] inp = br.readLine().split(" ");
-            int opr = Integer.parseInt(inp[0]);
-            if(opr == 1){
-                long ele = Long.parseLong(inp[1]);
-//                if(st.isEmpty()){
-                    st.push(ele);
-//                }else{
-//                    if(st.peek() > ele){
-//                        long temp = st.pop();
-//                        st.push(ele);
-//                        st.push(temp);
-//                    }else{
-//                        st.push(ele);
-//                    }
-//                }
-            }else if(opr == 2){
-                st.pop();
-            }else {
-                printMaxElementInStack(st);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        Stack<Integer> st = new Stack<Integer>();
+        int max = Integer.MIN_VALUE;
+        while(size -- > 0){
+            int n = sc.nextInt();
+            if(n == 1){
+                int element = sc.nextInt();
+                max = Math.max(max,element);
+                st.push(max);
+
+            }
+            else if(n == 2){
+                if(!st.empty()){
+                    st.pop();
+                }
+                if(st.empty()){
+                    max = Integer.MIN_VALUE;
+                }
+                else{
+                    max = st.peek();
+                }
+            }
+            else if(n == 3){
+                System.out.println(st.peek());
             }
         }
-    }
-    public static void printMaxElementInStack(Stack<Long> st){
-        List<Long> arrl = new ArrayList<>();
-        while(!st.isEmpty()) {
-            arrl.add(st.pop());
-        }
-        Collections.sort(arrl);
-        for(Long ele: arrl){
-            st.push(ele);
-        }
-        System.out.println();
     }
 }
