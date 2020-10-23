a926a4cd465ffc31122b1bfa531f334577dbda4e
ParichayPai <parichaypai393@gmail.com>
2020-10-23 11:51:58 +0530

    numberOfWays

--- src/com/recursion/numberOfWays.java
+++ src/com/recursion/numberOfWays.java
@@ -1,5 +1,33 @@
 package com.recursion;
 
+/*
+Find number of ways an integer N can be represented as a sum of unique natural numbers.
+Input
+First line contain an integer T denoting number of test cases. Each test case contains a single integer N.
+
+Constraint:-
+1 <= T <= 100
+0 <= N <= 120
+Output
+Print a single integer containing number of ways.
+Sample input
+4
+6
+1
+4
+2
+
+Sample output:-
+4
+1
+2
+1
+
+Explanation:-
+TestCase1:-
+6 can be represented as (1, 2, 3), (1, 5), (2, 4), (6)
+ */
+
 import java.util.Scanner;
 
 public class numberOfWays {
