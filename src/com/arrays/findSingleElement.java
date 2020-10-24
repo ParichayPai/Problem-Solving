bc708b99810cacb3488a443e7602df3d0d1b572e
ParichayPai <parichaypai393@gmail.com>
2020-10-24 20:16:50 +0530

    trickyNumber

---
+++ src/com/arrays/findSingleElement.java
@@ -0,0 +1,22 @@
+package com.arrays;
+
+import java.util.Scanner;
+
+public class findSingleElement {
+    public static int findSingle(int[] arr, int arr_size) {
+        int res = arr[0];
+        for (int i = 1; i < arr_size; i++)
+            res = res ^ arr[i];
+
+        return res;
+    }
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Element occurring once is " + findSingle(arr, n));
+    }
+}
