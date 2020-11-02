9d34dcad89f648f1af454df31a60c52f23021ffa
ParichayPai <parichaypai393@gmail.com>
2020-11-02 20:35:23 +0530

    QuickSort

--- src/com/Sorts/QuickSort.java
+++ src/com/Sorts/QuickSort.java
@@ -7,43 +7,14 @@ public class QuickSort {
     public static long[] quickSort(long[] arr, int start, int end){
         if(start>=end)
             return arr;
-        // int partitionIndex = randomizedPartition(arr, start, end);
-        // quickSort(arr, start, partitionIndex - 1);
-        // quickSort(arr, partitionIndex + 1, end);
-        // return arr;
+
         int partitionIndex = Partition(arr, start, end);
         quickSort(arr, start, partitionIndex);
         quickSort(arr, partitionIndex+1, end);
         return arr;
     }
 
-    // static int randomizedPartition(int[] arr, int start, int end) {
-//       int length = end-start + 1;
-//       int pivotIndex = start + (int)Math.floor(Math.random()*length);
-
-//       int temp = arr[end];
-//       arr[end] = arr[pivotIndex];
-//       arr[pivotIndex] = temp;
-
-//       return Partition(arr, start, end);
-// }
-
     static int Partition(long[] arr, int start, int end){
-        // int pivot = arr[end];
-        // int length = end - start+1;
-        // int partitionIndex = start;
-        // for(int i=0;i<length-1;++i){
-        //       if(arr[start+i] < pivot){
-        //             int temp = arr[start + i];
-        //             arr[start+i] = arr[partitionIndex];
-        //             arr[partitionIndex] = temp;
-        //             partitionIndex++;
-        //       }
-        // }
-        // int temp = arr[end];
-        // arr[end] = arr[partitionIndex];
-        // arr[partitionIndex] = temp;
-        // return partitionIndex;
         int i = start-1, j=end+1;
         long pivot = arr[start];
         // while(i<=j){
