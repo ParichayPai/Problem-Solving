068b60c41bdae36dec7823282f5ffd08eac233a0
ParichayPai <parichaypai393@gmail.com>
2020-11-02 20:29:53 +0530

    MergeSort

--- src/com/Sorts/MergeSort.java
+++ src/com/Sorts/MergeSort.java
@@ -21,14 +21,14 @@ public class MergeSort {
         int sorted2size = end - mid;
         long[] sorted1 = new long[sorted1size];
         long[] sorted2 = new long[sorted2size];
-        for(int i=0;i<sorted1size;i++){
+        for(int i = 0; i < sorted1size; i++){
             sorted1[i] = arr[start + i];
         }
-        for(int i=0;i<sorted2size;i++){
+        for(int i = 0; i < sorted2size; i++){
             sorted2[i] = arr[mid+1 + i];
         }
-        int i=0, j=0, k=start;
-        while(i<sorted1size && j<sorted2size){
+        int i = 0, j = 0, k = start;
+        while(i < sorted1size && j < sorted2size){
             if(sorted1[i] < sorted2[j]){
                 arr[k++] = sorted1[i++];
             }
