26ce8175d287fcb3f1dd9f8660c62508af210785
ParichayPai <parichaypai393@gmail.com>
2020-11-02 20:24:28 +0530

    InsertionSort

--- src/com/Sorts/InsertionSort.java
+++ src/com/Sorts/InsertionSort.java
@@ -1,29 +1,32 @@
 package com.Sorts;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 class InsertionSort{
+    public static void insertionSort(int[] arr, int n){
+        for(int i = 0; i < n; i++){
+            int pivot = arr[i];
+            int j = i-1;
+            while(j >= 0 && arr[j] > pivot){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = pivot;
+        }
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for(int k = 0; k < n; k++){
-            int m = sc.nextInt();
-            int[] arr = new int[m];
-            for(int i = 0; i < m; i++)
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++)
                 arr[i] = sc.nextInt();
-            for(int i = 0; i < m; i++){
-                int pivot = arr[i];
-                int j= i-1;
-                while(j>=0 && arr[j] > pivot){
-                    arr[j+1] = arr[j];
-                    j--;
-                }
-                arr[j+1] = pivot;
-            }
-            for(int i=0;i<m;i++){
-                System.out.print(arr[i]+" ");
-            }
-            System.out.print("\n");
+            insertionSort(arr, n);
         }
     }
 }
