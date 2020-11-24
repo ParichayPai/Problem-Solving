9a6355adef307f883a10fbf96cbd7f10d09ff81c
ParichayPai <parichaypai393@gmail.com>
2020-11-24 15:48:59 +0530

    BST Implementation

--- src/com/Tree_Based/bst.java
+++ src/com/Tree_Based/bst.java
@@ -76,10 +76,88 @@ public class bst {
         postOrderTRecursive(root);
     }
 
-    public int deleteNode(int ele){
+    private btNode searchParent(btNode root, int ele){
+        if(root == null){
+            return null;
+        }
+        if((root.left != null && root.left.data == ele) || (root.right!= null && root.right.data == ele)){
+            return root;
+        }else{
+            int currentNodeData = root.data;
+            if(currentNodeData >= ele){
+                return searchParent(root.left, ele);
+            } else {
+                return searchParent(root.right, ele);
+            }
+        }
+    }
+
+    private Integer singleChildDelete(int ele){
+        btNode parent = searchParent(root, ele);
+        boolean isDeleted = false;
+        if(parent != null){
+            if(parent.left != null && parent.left.data == ele){
+                parent.left = null;
+                isDeleted = true;
+
+            }else if(parent.right != null && parent.right.data == ele){
+                parent.right = null;
+                isDeleted = true;
+            }
+        }
+        return isDeleted ? ele : null;
+    }
+
+    private Integer noChildDelete(btNode parent, int ele){
+        boolean isDeleted = false;
+        if(parent != null){
+            if(parent.left != null && parent.left.data == ele){
+                parent.left = null;
+            }else if(parent.right != null && parent.right.data == ele){
+                parent.right = null;
+            }
+        }
+        return ele;
+    }
+
+    public Integer deleteNode(int ele){
+        btNode parent = searchParent(root, ele);
+        if(parent != null){
+            if(parent.left != null && parent.left.data == ele){
+                if(parent.left.left == null && parent.left.right == null){
+                    noChildDelete(parent, ele);
+                }else if(!(parent.left.left != null && parent.left.right != null)){
+                    singleChildDelete(ele);
+                }else{
+                    btNode inOrderSuccessor = findInOrderSuccessor(root, ele);
+                    btNode inOrderSuccessorParent = searchParent(root, inOrderSuccessor.data);
+                    swap(parent.left, inOrderSuccessor);
+                    return handleDeleteForInOrderSuccessor(inOrderSuccessorParent, inOrderSuccessor);
+                }
+            }else if(parent.right != null && parent.right.data == ele){
+                if(parent.right.left == null && parent.right.right == null){
+                    noChildDelete(parent, ele);
+                }
+//                else if(!(parent.right.left != null && parent.left.right != null)){
+//                    singleChildDelete(ele);
+//                }else{
+//
+//                }
+            }
+        }
+        return  null;
+    }
+
+    private int handleDeleteForInOrderSuccessor(btNode inOrderSuccessorParent, btNode inOrderSuccessor) {
         return 0;
     }
 
+    private void swap(btNode left, btNode inOrderSuccessor) {
+    }
+
+    private btNode findInOrderSuccessor(btNode root, int ele) {
+        return null;
+    }
 
 }
 
