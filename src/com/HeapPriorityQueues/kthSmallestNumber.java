34539bba6863377118d0891e807e12a348d7be36
ParichayPai <parichaypai393@gmail.com>
2021-01-20 00:06:57 +0530

    kthSmallestNumber

--- src/com/HeapPriorityQueues/kthSmallestNumber.java
+++ src/com/HeapPriorityQueues/kthSmallestNumber.java
@@ -3,10 +3,7 @@ package com.HeapPriorityQueues;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class kthSmallestNumber {
 //    public static void main(String[] args) throws IOException {
@@ -46,7 +43,6 @@ public class kthSmallestNumber {
                 pq.add(A.get(i));
             }
         }
-
         return pq.peek();
     }
 
